8eafa94c2f7f223ac6d8c15320e68f99f8b2d3c4
SHIU47 <ss23083937@gmail.com>
2026-01-14 22:37:09 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,6 +2,14 @@ const CACHE_NAME = 'hvac-v1';
 const ASSETS = [
   './',
   './index.html',
+  './Clean room master.html',
+  './冰機換算水泵.html',
+  './冰水管路計算.html',
+  './冷卻水塔補水量計算.html',
+  './空氣性質分析.html',
+  './空氣線圖Pro 計算器.html',
+  './空調專業換算工具.html',
+  './風管計算.html',
   // 如果你有其他的 css 或 js 檔案，請在下面列出路徑
   // './style.css',
   // './script.js'
